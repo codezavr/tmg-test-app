5bbe0005803fc041261e55d8a8d7865bd3956ee9
Rodion Adamenko <codezavr@gmail.com>
2021-07-06 18:26:16 +0300

    * - Change interfaces / types

--- src/data/messages.tsx
+++ src/data/messages.tsx
@@ -1,15 +1,5 @@
-export interface IMessage {
-    name: string | undefined;
-    message: string
-}
-
-export type IMessages = IMessage[];
+export type IMessages = string[];
 
 export interface IUser {
     name: string
 }
-
-export const LOGGED_USER: IUser = { name: '' };
-
-export const MESSAGES: IMessages = [
-];
